f25e3d392a5f4894b68e1e25515149b50e218aae
Grigory Vladimiro <grigori.vladimiro@gmail.com>
2025-03-08 22:40:48 +0100

    DNA_transcription_class

--- rna_transcription/src/main/java/dev/grigory/rna_transcription/DnaTranscription.java
+++ rna_transcription/src/main/java/dev/grigory/rna_transcription/DnaTranscription.java
@@ -1,5 +1,19 @@
 package dev.grigory.rna_transcription;
 
 public class DnaTranscription {
+    public String transcribe(String dna) {
+        StringBuilder rna = new StringBuilder();
 
+        for (char nucleotide : dna.toCharArray()) {
+            rna.append(switch (nucleotide) {
+                case 'G' -> 'C';
+                case 'C' -> 'G';
+                case 'T' -> 'A';
+                case 'A' -> 'U';
+                default -> throw new IllegalArgumentException("Invalid DNA nucleotide: " + nucleotide);
+            });
+        }
+
+        return rna.toString();
+    }
 }
